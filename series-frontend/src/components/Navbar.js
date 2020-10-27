90cde13815218f07efe81366c572078362beafd4
gijo57 <pekka-1992@hotmail.com>
2020-10-27 23:42:04 +0100

    initial set up for express app

--- series-frontend/src/components/Navbar.js
+++ series-frontend/src/components/Navbar.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-
         <div className="navbar">
             <Link to="/">Home</Link>
             <div className="dropdown">
